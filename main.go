434829762ba7841abbd6e41b22dacb70aebfa648
matt-cale-do <mcale@digitalocean.com>
2021-12-20 10:51:07 -0600

    fix/try: does AP block runtime casue that is awesome

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"runtime"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
-			"os":      runtime.GOOS,
+			// "os":      runtime.GOOS,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
